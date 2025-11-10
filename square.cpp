d0cf797b2a607e696ad0bc1a35aac8f064508fae
gowthamimadde1329 <gowthamimadde13@gmail.com>
2025-11-10 19:06:14 +0530

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int x,s;
+	printf("enter any number");
+	scanf("%d",&x);
+	s=sqr+(x);
+	printf("square root is=%d",s);
+	return 0;
+}
---
+++ swaping.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a and b values");
+	scanf("%d%d",&a,&b);
+	printf("before swaping=%d",a,b);
+	a=a*b
+	b=a/b
+	a=a/b
+	printf("\nafter swaping a=%d b=%d",a,b);
+	return 0;
+}
